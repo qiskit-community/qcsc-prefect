79ecf4f1ce7fa5f72b7be2cfd851f2e8860ce23d
Hitomi Takahashi <HITOMI@jp.ibm.com>
2026-02-13 16:06:49 +0900

    Fix test

--- packages/hpc-prefect-executor/src/hpc_prefect_executor/miyabi/__init__.py
+++ packages/hpc-prefect-executor/src/hpc_prefect_executor/miyabi/__init__.py
@@ -1,4 +1,15 @@
-from .from_blocks import run_miyabi_job_from_blocks
+from __future__ import annotations
+
+from typing import Any
+
 from .run import MiyabiRunResult, run_miyabi_job
+
+
+async def run_miyabi_job_from_blocks(*args: Any, **kwargs: Any):
+    # Lazy import avoids cycles with hpc_prefect_executor.from_blocks.
+    from .from_blocks import run_miyabi_job_from_blocks as _run_miyabi_job_from_blocks
+
+    return await _run_miyabi_job_from_blocks(*args, **kwargs)
+
 
 __all__ = ["MiyabiRunResult", "run_miyabi_job", "run_miyabi_job_from_blocks"]
